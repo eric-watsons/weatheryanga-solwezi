59de6798b0c17cb9405a67015d50aa57b749e048
eric-watsons <watsonkayumb@gmail.com>
2024-11-10 01:22:28 +0200

    Added Toggle Search in Mobile Devices

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,29 @@
  */
 
 'use strict';
+
+import { fetchData, url } from "./api.js";
+import * as module from "./module.js";
+
+/**
+ * Add event listener on multiple elements
+ * @param {NodeList} elements Elements node array
+ * @param {string} eventType Event type e.g.: "click", "mouseover"
+ * @param {Function} callback Callback function
+ */
+const addEventOnElements = function(elements, eventType, callback) {
+    for (const element of elements) element.addEventListener(eventType, callback);
+}
+
+/**
+ * Toggle search in mobile devices
+ */
+const searchView = document.querySelector("[data-search-view]");
+const searchTogglers = document.querySelectorAll("[data-search-toggler]");
+
+const toggleSearch = () => searchView.classList.toggle("active");
+addEventOnElements(searchTogglers, "click", toggleSearch);
+
+/**
+ * SEARCH INTERGRATION
+ */
