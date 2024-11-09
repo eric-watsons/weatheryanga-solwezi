bf1854b9794766300183e0dd0ac0d55972613dda
eric-watsons <watsonkayumb@gmail.com>
2024-11-09 23:24:26 +0200

    Added Module js

--- assets/js/api.js
+++ assets/js/api.js
@@ -4,3 +4,41 @@
  * @copyright codewithsadee 2023 All rights reserved
  * @author codewithsadee <mohammadsadee24@gmail.com>
  */
+
+'use strict';
+
+const api_key = "bfa2c6ba9d28858722774fc29a57a440";
+
+/**
+ * fetch data from server
+ * @param {string} URL API url
+ * @param {Function} callback callback
+ */
+
+export const fetchData = function(URL, callback) {
+    fetch('${URL}&appid=${api-key')
+    .then(res => res.json())
+    .then(data => callback(data));
+}
+
+export const url = {
+    currentweather(lat, lon){
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+    },
+    forecast(lat, lon){
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+    },
+    airpollution(lat, lon) {
+        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    },
+    reverseGeo(lat, lon) {
+        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    },
+    /**
+     * 
+     * @param {string} query search query e.g "Solwezi", "Lusaka"
+     */
+    geo(query) {
+        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    }
+}
